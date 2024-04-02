e22f9eed6c5150e30df3fe8584d8d6410d43a292
Jon P <jon.pillay@gmail.com>
2024-04-02 22:09:42 +0100

    basic experimentation with labels and grid

--- isp-gui.py
+++ isp-gui.py
@@ -5,9 +5,15 @@ import ttkbootstrap as tkb
 root = tkb.Window(themename="solar")
 
 root.title("InvoicesPY")
-root.geometry('600x400')
+root.geometry('1000x800')
 
-test_label = tkb.Label(text="We are live!")
-test_label.pack(pady=50)
+test_label = tkb.Label(root, text="We are live!")
+test_label.grid(column=2, row=1)
+
+test_label2 = tkb.Label(root, text="here though", bootstyle='danger')
+test_label2.grid(column=1, row=0)
+
+test_label3 = tkb.Label(root, text="here next", bootstyle='danger')
+test_label3.grid(column=1, row=1)
 
 root.mainloop()
