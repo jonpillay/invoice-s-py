ccfba1a5c9c798031f95a44da716c311352fe23a
Jon P <jon.pillay@gmail.com>
2024-04-09 17:41:41 +0100

    getCSVfile in CsvHelpers modified to read transactions page and return matches and also entries where matches not found. Regex expressions put in env to hide possible sensitive business data

--- ispCsvHelpers.py
+++ ispCsvHelpers.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -8,6 +8,11 @@ from tkinter import filedialog
 import csv
 
 def getCSVfile():
+
+  matchList = []
+  errorList= []
+  count = 0
+
   filename = filedialog.askopenfilename(
     initialdir=os.getenv("CSV_INITIAL_PATH"),
     title="Open Invoice",
@@ -16,6 +21,27 @@ def getCSVfile():
 
   with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file)
-
+    count = 0
     for entry in csv_reader:
-      print(entry)
+      count += 1
+      matchFound = False
+      for el in entry:
+        match = re.findall(os.getenv("CSV_TRANSACTION_REGEX"), el)
+        if match:
+          matchList.append(match[0])
+          matchFound = True
+      if matchFound == False:
+        errorList.append(entry)
+
+      # if len(matches) == 0:
+      #   errorList.append(entry)
+      # else:
+      #   for match in matches:
+      #     matchList.append(match)
+
+    print(count)
+    print(len(matchList))
+    print(len(errorList))
+    
+    for error in errorList:
+      print(error)
