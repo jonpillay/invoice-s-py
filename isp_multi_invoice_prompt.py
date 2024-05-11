d1935c454a46c99242a76d3538a5fe299cebc789
Jon P <jon.pillay@gmail.com>
2024-05-11 10:49:35 +0100

    multi invoice prompt window now renders size dynamically dependent on the amount of entries it has.

--- isp_multi_invoice_prompt.py
+++ isp_multi_invoice_prompt.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as tkb
 
-from isp_treeviews import renderPromptInvoices
+from isp_treeviews import renderPromptInvoices, renderPromptMulitTransactions
 
 def openMultiInvoicePrompt(root, transaction, invoiceList):
 
@@ -9,20 +9,36 @@ def openMultiInvoicePrompt(root, transaction, invoiceList):
     promptWindow.title('Invoices Match?')
     promptWindow.geometry('800x600')
 
-    promptWindow.rowconfigure(0, weight=1)
-    promptWindow.rowconfigure(1, weight=20)
-    promptWindow.columnconfigure(0, weight=1)
+    def updateWindowHeight():
+      
+      height = sum([widget.winfo_height() for widget in promptWindow.winfo_children()])
+
+      print(f'This is the height {height}')
+
+      promptWindow.geometry(f'800x{height}')
+
+    # promptWindow.rowconfigure(0, weight=1)
+    # promptWindow.rowconfigure(1, weight=40)
+    # promptWindow.columnconfigure(0, weight=1)
 
     title_label = tkb.Label(promptWindow, text=f"Multiple Invoice Transaction", background='red')
-    title_label.grid(row=0, column=0, sticky='nesw')
+    # title_label.grid(row=0, column=0, sticky='nesw')
+    title_label.pack(side='top')
 
     main_frame = tkb.Frame(promptWindow)
-    main_frame.grid(row=1, column=0, sticky='nesw')
+    # main_frame.grid(row=1, column=0, sticky='nesw')
+    main_frame.pack(side='top', expand=True)
 
     main_frame.rowconfigure(0, weight=1)
     main_frame.rowconfigure(1, weight=1)
     main_frame.columnconfigure(0, weight=1)
 
-    renderPromptInvoices(promptWindow, invoiceList, 0, 0)
+    renderPromptMulitTransactions(main_frame, [transaction], 0, 0)
 
-    promptWindow.wait_window(main_frame)
+    renderPromptInvoices(main_frame, invoiceList, 1, 0)
+
+    promptWindow.update()
+
+    updateWindowHeight()
+
+    promptWindow.wait_window()
--- isp_treeviews.py
+++ isp_treeviews.py
@@ -2,7 +2,7 @@ from tkinter import ttk
 
 def renderPromptInvoices(parentWindow, invoiceList, gridRow, gridColumn):
 
-  invoiceTable = ttk.Treeview(parentWindow, columns = ('invoice_num', 'issued_to', 'amount', 'date_issued', 'notes'), show='headings')
+  invoiceTable = ttk.Treeview(parentWindow, columns = ('invoice_num', 'issued_to', 'amount', 'date_issued', 'notes'), show='headings', height=len(invoiceList))
 
   invoiceTable.heading('invoice_num', text="Invoice Num")
   invoiceTable.heading('issued_to', text='Customer')
@@ -10,14 +10,14 @@ def renderPromptInvoices(parentWindow, invoiceList, gridRow, gridColumn):
   invoiceTable.heading('date_issued', text='Issued On')
   invoiceTable.heading('notes', text='Notes')
 
-  invoiceTable.grid(row=gridRow, column=gridColumn)
+  invoiceTable.pack()
 
   for i in range(len(invoiceList)):
     invoiceTable.insert(parent='', index=i, values=(invoiceList[i].invoice_num, invoiceList[i].issued_to, invoiceList[i].amount, invoiceList[i].date_issued, invoiceList[i].error_notes))
 
 def renderPromptMulitTransactions(parentWindow, transactionList, gridRow, gridColumn):
 
-  multiTransactionTable = ttk.Treeview(parentWindow, columns=('low_invoice', 'high_invoice', 'amount', 'paid_on', 'paid_by', 'payment_method', 'og_string'))
+  multiTransactionTable = ttk.Treeview(parentWindow, columns=('low_invoice', 'high_invoice', 'amount', 'paid_on', 'paid_by', 'payment_method', 'og_string'), show='headings', height=len(transactionList))
 
   multiTransactionTable.heading('low_invoice', text='From Invoice')
   multiTransactionTable.heading('high_invoice', text='To Invoice')
@@ -27,7 +27,7 @@ def renderPromptMulitTransactions(parentWindow, transactionList, gridRow, gridCo
   multiTransactionTable.heading('payment_method', text='Method')
   multiTransactionTable.heading('og_string', text='Description')
 
-  multiTransactionTable.grid(row=gridRow, column=gridColumn)
+  multiTransactionTable.pack()
 
   for i in range(len(transactionList)):
 
